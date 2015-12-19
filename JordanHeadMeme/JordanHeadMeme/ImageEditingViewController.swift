4d1c04489c49c9ade4a5a0b37fd1e7779c4fa1d1
dokun1 <david@okun.io>
2015-12-19 20:49:26 +0000

    All gesture Recognizers working now, just need to think about screens with very small head, not a good UX for making them bigger

--- JordanHeadMeme/JordanHeadMeme/ImageEditingViewController.swift
+++ JordanHeadMeme/JordanHeadMeme/ImageEditingViewController.swift
@@ -14,12 +14,13 @@ protocol ImageEditingViewControllerDelegate: class {
     func controllerDidCancel(controller: ImageEditingViewController)
 }
 
-class ImageEditingViewController: UIViewController {
+class ImageEditingViewController: UIViewController, UIGestureRecognizerDelegate {
     var uneditedImage: UIImage!
     var correctedImage: UIImage!
     var screensizeImage: UIImage!
     var imageView: UIImageView!
     weak var delegate: ImageEditingViewControllerDelegate?
+    var generatedHeads: [JordanHead] = [JordanHead]()
     
     // MARK: override methods
     
@@ -31,15 +32,20 @@ class ImageEditingViewController: UIViewController {
         imageView = UIImageView.init(image: screensizeImage)
         imageView.frame = CGRectMake(0, 0, screensizeImage.size.width, screensizeImage.size.height)
         imageView.center = self.view.center
+        imageView.userInteractionEnabled = true
         self.view.addSubview(imageView)
     }
     
     override func viewDidAppear(animated: Bool) {
         if let results = processImage() {
+            var counter = 0
             for r in results {
+                counter++
                 let face: CIFaceFeature = r as! CIFaceFeature
                 if face.hasLeftEyePosition && face.hasRightEyePosition && face.hasMouthPosition {
-                    imageView.addSubview(drawJordanHead(getRectForDrawingHead(face), feature: face))
+                    let jordanHead = drawJordanHead(getRectForDrawingHead(face), feature: face, tag: counter)
+                    imageView.addSubview(jordanHead.imageView)
+                    generatedHeads.append(jordanHead)
                 }
             }
         }
@@ -47,18 +53,101 @@ class ImageEditingViewController: UIViewController {
     
     // MARK: UI Drawing Methods
 
-    func drawJordanHead(drawRect: CGRect, feature: CIFaceFeature) -> UIImageView {
-        let jordanHead = UIImageView.init(image: UIImage.init(named: "jordanHead.png"))
-        jordanHead.frame = drawRect
-        jordanHead.contentMode = .ScaleAspectFit
-        jordanHead.backgroundColor = UIColor.clearColor()
-        jordanHead.transform = CGAffineTransformMakeRotation(CGFloat(feature.faceAngle * Float(M_PI/180)))
+    func drawJordanHead(drawRect: CGRect, feature: CIFaceFeature, tag: Int) -> JordanHead {
+        let jordanHead = JordanHead()
+        let jordanHeadImage = UIImageView.init(image: UIImage.init(named: "jordanHead.png"))
+        jordanHeadImage.frame = drawRect
+        jordanHeadImage.contentMode = .ScaleAspectFit
+        jordanHeadImage.backgroundColor = UIColor.clearColor()
         if feature.hasRightEyePosition && feature.hasLeftEyePosition {
             if feature.rightEyePosition.y > feature.leftEyePosition.y {
-                jordanHead.transform = CGAffineTransformMakeScale(-1, 1)
+                jordanHeadImage.transform = CGAffineTransformMakeScale(-1, 1)
+                jordanHead.facingRight = false
             }
         }
+        jordanHeadImage.transform = CGAffineTransformMakeRotation(CGFloat(feature.faceAngle * Float(M_PI/180)))
+        jordanHeadImage.userInteractionEnabled = true
+        
+        addGestureRecognizers(jordanHeadImage)
+        jordanHeadImage.tag = tag
+        jordanHead.imageView = jordanHeadImage
+        jordanHead.faceID = tag
+        jordanHead.faceFeature = feature
         return jordanHead
+    }
+    
+    func addGestureRecognizers(head: UIImageView) {
+        let panner = UIPanGestureRecognizer.init(target: self, action: Selector("panGestureActivated:"))
+        panner.delegate = self
+        head.addGestureRecognizer(panner)
+        
+        let doubleTapper = UITapGestureRecognizer.init(target: self, action: Selector("doubleTapGestureActivated:"))
+        doubleTapper.numberOfTouchesRequired = 1
+        doubleTapper.numberOfTapsRequired = 2
+        doubleTapper.delegate = self
+        head.addGestureRecognizer(doubleTapper)
+        
+        let pincher = UIPinchGestureRecognizer.init(target: self, action: Selector("pinchGestureActivated:"))
+        pincher.delegate = self
+        head.addGestureRecognizer(pincher)
+        
+        let rotator = UIRotationGestureRecognizer.init(target: self, action: Selector("rotationGestureActivated:"))
+        rotator.delegate = self
+        head.addGestureRecognizer(rotator)
+    }
+    
+    // MARK: Gesture Recognizer Targets
+    
+    func rotationGestureActivated(recognizer: UIRotationGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = CGAffineTransformRotate(view.transform, recognizer.rotation)
+            recognizer.rotation = 0
+        }
+    }
+    
+    func panGestureActivated(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.imageView)
+        if let view = recognizer.view {
+            view.center = CGPointMake(view.center.x + translation.x, view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+    }
+    
+    func doubleTapGestureActivated(recognizer: UITapGestureRecognizer) {
+        if let imageView = recognizer.view as? UIImageView {
+            let tag = imageView.tag
+            var tappedHead: JordanHead?
+            for head in generatedHeads {
+                if head.faceID == tag {
+                    tappedHead = head
+                    break
+                }
+            }
+            guard (tappedHead != nil) else {
+                return
+            }
+            // wtf why isnt this working
+            if tappedHead?.facingRight == true {
+                tappedHead?.imageView.transform = CGAffineTransformMakeScale(1, 1)
+                tappedHead?.facingRight = true
+            } else {
+                tappedHead?.imageView.transform = CGAffineTransformMakeScale(-1, 1)
+                tappedHead?.facingRight = false
+            }
+        }
+    }
+    
+    func pinchGestureActivated(recognizer: UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = CGAffineTransformScale(view.transform, recognizer.scale, recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
+    
+    // MARK: UIGestureRecognizerDelegate Methods
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
     // MARK: Face detection methods
---
+++ JordanHeadMeme/JordanHeadMeme/JordanHead.swift
@@ -0,0 +1,17 @@
+//
+//  JordanHead.swift
+//  JordanHeadMeme
+//
+//  Created by David Okun on 12/19/15.
+//  Copyright © 2015 David Okun, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JordanHead: UIView {
+    var imageView = UIImageView()
+    var facingRight = true
+    var faceID = Int()
+    var faceFeature = CIFaceFeature()
+}
